827a4ba7c556f6a4b9656ccb6595bc1ec445d5d8
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-05-04 11:57:52 +0530

    Add eslint config

--- src/App.test.js
+++ src/App.test.js
@@ -9,7 +9,7 @@ test('button has correct initial color', () => {
   expect(colorButton).toHaveStyle({ backgroundColor: 'MediumVioletRed' });
   fireEvent.click(colorButton);
   expect(colorButton).toHaveStyle({ backgroundColor: 'MidnightBlue' });
-  expect(colorButton.textContent).toBe('Change to Medium Violet Red');
+  expect(colorButton).toHaveTextContent('Change to Medium Violet Red');
 });
 
 test('initial conditions', () => {
